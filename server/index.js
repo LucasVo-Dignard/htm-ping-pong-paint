3df90ffb877ca28e79e2afb19f17cf925feaeab2
djratpack <manndonk@gmail.com>
2026-05-23 14:01:55 -0400

    Laid out basis of 2 different webpages for PC and mobile

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const { createServer } = require('http');
 const path = require('path');
+
 const app = express();
-const PORT = 3000;
+const httpServer = createServer(app);
+const PORT = 80; // Default HTTP port: 80, Default HTTPS port: 443
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/client', express.static(path.join(__dirname, '..', 'client')));
 
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/pc', 'index.html'));
+});
+
+app.get('/mobile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/mobile', 'index.html'));
+});
+
+httpServer.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
