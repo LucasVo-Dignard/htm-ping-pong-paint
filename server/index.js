c9f7eb17189d4eff4c7f3478c89e83f8ca445927
Distinguished-Monke <enzojung82@gmail.com>
2026-05-23 14:22:07 -0400

    mobile page

--- server/index.js
+++ server/index.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const { createServer } = require('http');
 const path = require('path');
-
 const app = express();
-const httpServer = createServer(app);
-const PORT = 80; // Default HTTP port: 80, Default HTTPS port: 443
+const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/client', express.static(path.join(__dirname, '..', 'client')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/pc', 'index.html'));
-});
-
-app.get('/mobile', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/mobile', 'index.html'));
-});
-
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
