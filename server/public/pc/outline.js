47f206c23256ce7e93580a4edb7dc67a2e2078ea
djratpack <manndonk@gmail.com>
2026-05-23 15:44:18 -0400

    Added contents to the index.html page for pc

--- server/public/pc/outline.js
+++ server/public/pc/outline.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 socket.on('code', (code) => {
-  console.log(code);
+  codeBlock.innerHTML = code.split('').map(c => `<div class="code-char">${c}</div>`).join('');
  });
+
+// dot.classList.add('connected');
+// statusText.textContent = 'Connected';
+// startBtn.disabled = false;
 
 socket.on('disconnect', () => {
   console.log("disconnected");
