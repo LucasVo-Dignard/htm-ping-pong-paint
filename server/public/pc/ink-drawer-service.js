5c7a75feb6b26ac77bcaf6dc0e0a9910c942fac2
LucasVo-Dignard <vodignardlucas@gmail.com>
2026-05-24 06:37:58 -0400

    changed splat size ranges

--- server/public/pc/ink-drawer-service.js
+++ server/public/pc/ink-drawer-service.js
@@ -1,6 +1,6 @@
 const HIT_Y_OFFSET = 0.5;
-const MIN_SPLAT_SIZE = 26;
-const MAX_SPLAT_SIZE = 36;
+const MIN_SPLAT_SIZE = 30;
+const MAX_SPLAT_SIZE = 56;
 
 (function () {
   const DEFAULT_PALETTE = [
