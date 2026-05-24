6151dd6b4e15bda00087221586a6eb155dc51269
LucasVo-Dignard <vodignardlucas@gmail.com>
2026-05-24 02:01:08 -0400

    added test route

--- server/index.js
+++ server/index.js
@@ -27,6 +27,10 @@ app.get('/testBall', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/pc', '3d_pingpong_threejs.html'));
 });
 
+app.get('/testMainPc', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/pc', 'test.html'));
+});
+
 app.get('/mobile', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/mobile', 'index.html'));
 });
