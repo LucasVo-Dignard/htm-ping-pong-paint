92f859e023a7bf47f757a23279fe5bce1656f9b4
don <mandon.kahn@gmail.com>
2026-05-24 07:31:31 -0400

    commit

--- server/public/pc/script.js
+++ server/public/pc/script.js
@@ -392,10 +392,7 @@ function updatePhysics(delta) {
     balls.forEach(ball => updateBallPhysics(ball, delta));
 }
 
-
-
 const clock = new THREE.Clock();
-
 
 function animate() {
     requestAnimationFrame(animate);
